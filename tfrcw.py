fdc6c891df774c455310dd2661f429d565edad6e
Nongkai Tian <2284312235@qq.com>
2024-10-25 21:37:19 +0800

    更新了tfrcw函数

--- tfrcw.py
+++ tfrcw.py
@@ -1,3 +1,7 @@
 def tfrcw(x=None,t=None,N=None,g=None,h=None,sigma=None,trace=None):
     if x is None and t is None and N is None and g is None and h is None and sigma is None and trace:
-        raise 
+        raise ValueError("At least 1 parameter required")
+    xrow = x.shape[0]
+    xcol = x.shape[1]
+    if xcol == 0 or xcol > 2:
+        raise ValueError("X must have one or two columns")
